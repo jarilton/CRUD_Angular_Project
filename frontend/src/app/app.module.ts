8152c0ecc5545442b0e1ca1167b40ba5e57c9ea5
Jarilton <jariltonpereira02@hotmail.com>
2021-11-24 17:38:59 -0300

    ajustes de palavras corretas pois escrevi components errado

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './componets/template/header/header.component';
+import { HeaderComponent } from './components/template/header/header.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { FooterComponent } from './componets/template/footer/footer.component';
-import { NavComponent } from './componets/template/nav/nav.component';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component';
 
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatCardModule } from '@angular/material/card';
