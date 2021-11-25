3250b8e24c0ccf3bd7546887cd5fc8962ea395a3
Jarilton <jariltonpereira02@hotmail.com>
2021-11-25 09:55:13 -0300

    Método criar produto

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,3 +1,4 @@
+import { Product } from './../product.model';
 import { ProductService } from '../product.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
@@ -9,6 +10,11 @@ import { Router } from '@angular/router';
 })
 export class ProductCreateComponent implements OnInit {
 
+  product: Product = {
+    name: '',
+    price: 0
+  }
+
   constructor(private productService: ProductService,
     private router: Router) { }
 
@@ -17,7 +23,10 @@ export class ProductCreateComponent implements OnInit {
   }
 
   createProduct(): void {
-    this.productService.showMessage('Produto criado com sucesso!')
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage('Produto criado com sucesso!')
+      this.router.navigate(['/products'])
+    })
   }
 
   cancel(): void {
